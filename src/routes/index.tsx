cf8eb4796fe843c7cd79dba9cf380503e4678461
Abourass <Abourassa@AssetVal.com>
2024-05-29 19:15:25 -0600

    chore: Add initial project files and configurations

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { A } from "@solidjs/router";
+
+export default function Home() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4">
+      <h1 class="max-6-xs text-5xl text-sky-700 font-thin uppercase my-16">
+        Cauldron
+      </h1>
+
+      <p class="my-4">
+        <span>Home</span>
+        {" - "}
+        <A href="/about" class="text-sky-600 hover:underline">
+          About Page
+        </A>{" "}
+      </p>
+    </main>
+  );
+}
