59f730c546da949b06d163fba8af3a3786b95e4b
Abourass <Abourassa@AssetVal.com>
2024-06-06 18:38:24 -0600

    ```text refactor: Update ladle module to support additional environment variable prefixes
    The ladle module has been updated to support additional environment variable prefixes. Now, in addition to the 'CAULDRON_' prefix, it also recognizes 'AQUA__' and 'AQUARIUM__' prefixes. This allows for more flexibility in retrieving environment variables based on different prefixes.
    
    This change improves the functionality and versatility of the ladle module, making it easier to retrieve the desired environment variables.

--- src/modules/ladle.ts
+++ src/modules/ladle.ts
@@ -1,10 +1,32 @@
 import ENV_FILE from "../assets/env.json"
 
-export default () => {
-  const cauldron_prefixed = ENV_FILE.filter((env) => env.name.startsWith('CAULDRON_'));
+type PrefixedRequest = 'aqua' | 'all';
+type RawRequest = `${PrefixedRequest}:raw`;
 
-  return cauldron_prefixed.map((env) => ({
-    ...env,
-    name: env.name.replace('CAULDRON_', ''),
-  }))
+/**
+ * Ladle
+ * 
+ * This module will read from the env.json file and:
+ * 
+ * - Default: Return all the environment variables that are prefixed with 'CAULDRON_'.
+ * - "aqua": Return all the environment variables that are prefixed with 'AQUA__' or `AQUARIUM__`.
+ * - "all": Return all the environment variables
+ * 
+ * Additionally, if they add `:raw` it will not clean the 'CAULDRON_' prefix from the environment variable names.
+ */
+export default <R extends undefined | PrefixedRequest | RawRequest | 'raw'>(requesting?: R) => {
+  const requested_prefixed = (requesting === 'all' || requesting === 'all:raw')
+    ? ENV_FILE
+    : (requesting === 'aqua' || requesting === 'aqua:raw')
+      ? ENV_FILE.filter((env) => env.name.startsWith('AQUA__') || env.name.startsWith('AQUARIUM__'))
+      : ENV_FILE.filter((env) => env.name.startsWith('CAULDRON_'));
+
+  return (requesting === 'raw') 
+    ? requested_prefixed
+    : (requesting?.endsWith(':raw'))
+      ? requested_prefixed
+      : requested_prefixed.map((env) => ({
+        ...env,
+        name: env.name.replace('CAULDRON_', ''),
+      }))
 }
